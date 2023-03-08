277b6598b112927128fac337a35a47b8ed0cdd4c
BacoMane <baco.mane.ba@outlook.com>
2023-03-07 19:10:27 -0500

    finally

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,11 +4,21 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!isValid(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -20,8 +30,7 @@ public_users.get('/',function (req, res) {
     });
     myPromise1.then((result) => {
         return res.status(200).send(result);
-});
-    
+    });
 });
 
 // Get book details based on ISBN
